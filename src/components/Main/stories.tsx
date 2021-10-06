810b1fff91aa9293c2993d6a0659289109c7935f
assoares383 <alexandressoares383@gmail.com>
2021-10-05 21:50:30 -0300

    setting up the storybook and writing stories

---
+++ src/components/Main/stories.tsx
@@ -0,0 +1,15 @@
+import { Story, Meta } from '@storybook/react'
+import Main from '.'
+
+export default {
+  title: 'Main',
+  component: Main
+} as Meta
+
+export const Basic: Story = (args) => <Main {...args} />
+Basic.args = {
+  title: 'title basic',
+  description: 'description basic'
+}
+
+export const Default: Story = (args) => <Main {...args} />
